e4689335f2b2062295d72187aec0e57caff89fc4
YangDR <ydrml@hotmail.com>
2018-11-25 12:17:17 +0800

    添加通用广播收发模块
    Signed-off-by: YangDR <ydrml@hotmail.com>

---
+++ MechDancer.Framework.Net/Remote/Modules/Multicast/CommonMulticast.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using MechDancer.Framework.Net.Dependency;
+using MechDancer.Framework.Net.Remote.Protocol;
+using MechDancer.Framework.Net.Remote.Resources;
+using static MechDancer.Framework.Net.Dependency.Functions;
+
+namespace MechDancer.Framework.Net.Remote.Modules.Multicast {
+	public sealed class CommonMulticast : AbstractModule, IMulticastListener {
+		private readonly Lazy<MulticastBroadcaster> _broadcaster;
+		private readonly Action<string, byte[]>     _action;
+
+		public CommonMulticast(Action<string, byte[]> action) {
+			_action      = action;
+			_broadcaster = Must<MulticastBroadcaster>(Host);
+		}
+
+		public void Broadcast(byte[] payload) =>
+			_broadcaster.Value.Broadcast(UdpCmd.Common, payload);
+
+		public IReadOnlyCollection<byte> Interest => InterestSet;
+
+		public void Process(RemotePacket remotePacket) {
+			var (name, _, _, payload) = remotePacket;
+			_action(name, payload);
+		}
+
+		public override bool Equals(object obj) => obj is CommonMulticast;
+		public override int  GetHashCode()      => Hash;
+
+		private static readonly int           Hash        = typeof(CommonMulticast).GetHashCode();
+		private static readonly HashSet<byte> InterestSet = new HashSet<byte> {(byte) UdpCmd.Common};
+	}
+}
