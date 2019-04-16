8d2ae385fcc5a0257007a505b22a320aa4060010
YdrMaster <ydrml@hotmail.com>
2019-04-16 17:20:22 +0800

    增加 TCP 示例

--- UserInterface/TestTcp.cs
+++ UserInterface/TestTcp.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Threading;
 using MechDancer.Common;
+using MechDancer.Framework.Net.Modules.Multicast;
 using MechDancer.Framework.Net.Modules.TcpConnection;
 using MechDancer.Framework.Net.Presets;
 using MechDancer.Framework.Net.Resources;
 
 namespace UserInterface {
 	public static class TestTcp {
-		public static void Test() {
+		public static void TestClient(string serverName) {
 			var hub = new RemoteHub("C#");
 			hub.Monitor.OpenAll();
 
@@ -17,7 +18,7 @@ namespace UserInterface {
 
 			while (true) {
 				var success = hub.Connect
-					("kotlin echo server", (byte) TcpCmd.Common,
+					(serverName, (byte) TcpCmd.Common,
 					 I => {
 						 Console.WriteLine("connected framework");
 						 while (true) {
@@ -33,5 +34,28 @@ namespace UserInterface {
 				else break;
 			}
 		}
+
+        public static void TestServer() {
+            var hub = new RemoteHub("C# Server",
+                additions: new ConnectionListener
+                     (interest: (byte)TcpCmd.Common,
+                      func: (name, I) => {
+                          Console.WriteLine("connected: {0}", name);
+                          while (true) {
+                              var sentence = I.Listen().GetString();
+                              if (sentence == "over") break;
+                              Console.WriteLine("server heared: {0}", sentence);
+                              I.Say(sentence.GetBytes());
+                          }
+                          Console.WriteLine("server heared over to break");
+                      }));
+            hub.Yell();
+
+            new Thread(() => {
+                while (true) hub.Invoke();
+            }).Start();
+
+            while (true) hub.Accept();
+        }
 	}
 }
